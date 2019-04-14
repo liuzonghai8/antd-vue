d59c32a048d9101dacf7dbeef9098d815950075d
LZH147258 <369613662@qq.com>
2019-04-14 18:06:34 +0800

    init

--- src/api/login.js
+++ src/api/login.js
@@ -1,4 +1,5 @@
 import { postAction } from './common'
 //根据用户名和密码登陆
-export const loginByUserName = (loginParam) => postAction('upms/sys/user/login2', loginParam)
+export const loginByUserName = (loginParam) => postAction('auth/login', loginParam)
+//export const loginByUserName = (loginParam) => postAction('upms/sys/user/query', loginParam)
 export const logoutServe = (token) => postAction('upms/sys/user/logout', token)
