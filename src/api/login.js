6b0e20eab94a8329a2ff7070d8e2a6b2517d5b55
liuzonghai8 <liuzonghai8@163.com>
2019-04-27 11:43:48 +0800

    init

--- src/api/login.js
+++ src/api/login.js
@@ -1,5 +1,5 @@
 import { postAction } from './common'
 //根据用户名和密码登陆
-export const loginByUserName = (loginParam) => postAction('auth/login', loginParam)
-//export const loginByUserName = (loginParam) => postAction('upms/sys/user/query', loginParam)
+//export const loginByUserName = (loginParam) => postAction('auth/login', loginParam)
+export const loginByUserName = (loginParam) => postAction('upms/sys/user/query', loginParam)
 export const logoutServe = (token) => postAction('upms/sys/user/logout', token)
